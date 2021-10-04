45903e965b25a107d391af31ee3de01366402eb1
Timothy <tsit20@stuy.edu>
2021-10-04 11:31:43 -0400

    added correct file.

--- switcheroo.c
+++ switcheroo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 int main(){
@@ -9,11 +10,29 @@ int main(){
 	for (num = 1; num<=9; num++){
 		array[num] = rand();
 	}
-	printf("(");
+	printf("Array: (");
 	for (num = 0; num<=8; num++){
 		printf("%d, ", array[num]);
 	}
 	printf("%d)\n", array[9]);
-	return 0;
+	int r_array[10];
+	int *a = array;
+	int *ra = r_array;
+	for (num = 0; num<=9; num++){
+		*(ra+num) = *(a+9-num);
+	}
+	printf("Reverse Array using *: (");
+	for (num = 0; num<=8; num++){
+		printf("%d, ", r_array[num]);
+	}
+	printf("%d)\n", r_array[9]);
+	for (num = 0; num<=9; num++){
+		r_array[num] = array[9-num];
+	}
+	printf("Reverse Array using []: (");
+	for (num = 0; num<=8; num++){
+		printf("%d, ", r_array[num]);
+	}
+	printf("%d)\n", r_array[9]);
 }
 
